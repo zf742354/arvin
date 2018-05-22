e6482f080faf80f09705a0d179453675db6c981a
a <310888511@qq.com>
2018-05-22 12:15:48 +0800

    5-22

--- js/5-19.js
+++ js/5-19.js
@@ -1,22 +1,23 @@
 $(function () {
-    $(".btn-top1").click(function(){
+
+    $(".btn-top1").click(function () {
 
         $('.right').hide();
         $('.left').show();
     });
 
-    $(".btn-top2").click(function(){
+
+    $(".btn-top2").click(function () {
 
         $('.left').hide();
         $('.right').show();
     });
 
 
-
     $("#btn").click(function () {
-        var i1 = $("#ipt1").val();
-        var i2 = $("#ipt2").val();
-        var i3 = $("#ipt3").val();
+        var i1 = $(".ipt1").val();
+        var i2 = $(".ipt2").val();
+        var i3 = $(".ipt3").val();
         var reg = /^[A-Za-z0-9]+$/;
         if (i1 === "" || i2 === "" || i3 === "") {
             alert("请完整输入")
@@ -25,14 +26,17 @@ $(function () {
                 if (reg.test(i2) && i2.length >= 6 && i2.length <= 12) {
                     if (i2 === i3) {
                         alert("注册成功");
-                        $("#ipt1").val("");
-                        $("#ipt2").val("");
-                        $("#ipt1").css({border: "none"});
-                        $("#ipt2").css({border: "none"});
-                        $("#ipt3").css({border: "none"});
-                        $("#ipt3").val("");
+                        $(".ipt1").val("");
+                        $(".ipt2").val("");
+                        $(".ipt1").css({border: "none"});
+                        $(".ipt2").css({border: "none"});
+                        $(".ipt3").css({border: "none"});
+                        $(".ipt3").val("");
                         localStorage.setItem('yonghm', i1);
                         localStorage.setItem('mima', i3);
+                        $('.right').hide();
+                        $('.left').show();
+
                     } else {
                         alert('两次输入不一致');
                         $("#ipt1").css({border: "none"});
@@ -47,22 +51,20 @@ $(function () {
                 }
             } else {
                 alert("输入不正确")
+
             }
         }
     });
 
 
     $("#btn-r").click(function () {
-
         var a = $('#ipt1-r').val();
         var b = $('#ipt2-r').val();
-
         if (a === '' || b === "") {
             alert("请完整输入")
         } else {
             var s = localStorage.getItem('yonghm');
             var w = localStorage.getItem('mima');
-
             if (s === a) {
                 if (w === b) {
                     window.location.href = 'https:www.baidu.com'
@@ -72,11 +74,8 @@ $(function () {
             } else {
                 alert("用户名不存在")
             }
-
         }
-
     })
 
 
 });
-
